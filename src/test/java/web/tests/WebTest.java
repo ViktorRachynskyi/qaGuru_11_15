032df9cbff0c178b351cf8dc0a8208d34c484ec0
Viktor Rachynskyi <viktor.rachynskyi@asquad.io>
2022-06-12 21:32:46 +0300

    fix tests

--- src/test/java/web/tests/WebTest.java
+++ src/test/java/web/tests/WebTest.java
@@ -3,13 +3,12 @@ package web.tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class WebTest extends BaseTest {
 
     @Test
-    void pageTitleTest() {
+    void webTest() {
         open("https://www.github.com");
         $("title").shouldHave(attribute("text",
                 "GitHub: Where the world builds software · GitHub"));
